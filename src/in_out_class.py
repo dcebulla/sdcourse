41353f92077b4ae691c9ab579e02adb2f762af32
Ihno Schrot <ihno.schrot@uni-heidelberg.de>
2022-03-03 13:41:56 +0100

    finished InClass

--- src/in_out_class.py
+++ src/in_out_class.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 class InClass:
-    def __init__(self, data_path="../data/"):
+    def __init__(self, data_path="data/"):
         self.data_path = data_path  # general data path
 
         # statistics related data fields
@@ -17,6 +17,7 @@ class InClass:
 
         # define methods to read
         self.read_functions = {
+            # Statistics
             "expec.t": lambda path: pd.read_csv(path, r"\s+"),
             "npop.t": lambda path: pd.read_csv(path, r"\s+"),
             "table.dat": lambda path: np.loadtxt(path, skiprows=1).T,
@@ -25,10 +26,41 @@ class InClass:
             "nstate_i.t": lambda path: np.loadtxt(path, skiprows=1),
         }
 
-    # def read_statistics_data(
-    #     self,
-    #     file_names = ["expec.t, npop.t, table.dat"]):
-    #     self.
+        # map file names to class attributes
+        self.names_to_attributes = {
+            # Statistics
+            "expec.t": "expec",
+            "npop.t": "npop",
+            "table.dat": "table",
+            # Numerics:
+            "efield.t": "efield",
+            "nstate_i.t": "nstate_i",
+        }
+
+    def read_data(
+        self,
+        file_names=[
+            "expec.t",
+            "npop.t",
+            "table.dat",
+            "efield.t",
+            "nstate_i.t",
+        ],
+    ):
+        # This function reads all the files whose names are given as input
+        for name in file_names:
+            full_path = self.data_path + name
+            vars(self)[self.names_to_attributes[name]] = self.read_functions[
+                name
+            ](full_path)
+
+    def read_statistics_data(self):
+        # for convenience, this call read_data only w. the files for statistics
+        self.read_data(file_names=["expec.t", "npop.t", "table.dat"])
+
+    def read_numerics_data(self):
+        # for convenience, this call read_data only with the files for numerics
+        self.read_data(file_names=["efield.t", "nstate_i.t"])
 
 
 class OutClass:
